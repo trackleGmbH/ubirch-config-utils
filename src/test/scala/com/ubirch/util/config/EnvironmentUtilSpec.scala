194e3680ede97bb0c8b1e21d4104e57206a7a5c9
Rueger <lotta.rueger@ubirch.com>
2022-01-14 17:35:53 +0100

    upgrade to scala 2.13.6 upgrade scalatest to 3.2.10 upgrade typesafe-config to 1.4.1

--- src/test/scala/com/ubirch/util/config/EnvironmentUtilSpec.scala
+++ src/test/scala/com/ubirch/util/config/EnvironmentUtilSpec.scala
@@ -1,266 +1,267 @@
 package com.ubirch.util.config
 
 
-import org.scalatest.{FeatureSpec, Matchers}
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
 
 /**
   * author: cvandrei
   * since: 2018-03-06
   */
-class EnvironmentUtilSpec extends FeatureSpec
+class EnvironmentUtilSpec extends AnyFeatureSpec
   with Matchers {
 
-  feature("POSTFIX_PROD") {
+  Feature("POSTFIX_PROD") {
 
-    scenario("value is _-prod_") {
+    Scenario("value is _-prod_") {
       EnvironmentUtil.POSTFIX_PROD shouldBe "-prod"
     }
 
   }
 
-  feature("POSTFIX_DEMO") {
+  Feature("POSTFIX_DEMO") {
 
-    scenario("value is _-demo_") {
+    Scenario("value is _-demo_") {
       EnvironmentUtil.POSTFIX_DEMO shouldBe "-demo"
     }
 
   }
 
-  feature("POSTFIX_DEV") {
+  Feature("POSTFIX_DEV") {
 
-    scenario("value is _-dev_") {
+    Scenario("value is _-dev_") {
       EnvironmentUtil.POSTFIX_DEV shouldBe "-dev"
     }
 
   }
 
-  feature("POSTFIX_LOCAL") {
+  Feature("POSTFIX_LOCAL") {
 
-    scenario("value is _-local_") {
+    Scenario("value is _-local_") {
       EnvironmentUtil.POSTFIX_LOCAL shouldBe "-local"
     }
 
   }
 
-  feature("isProd()") {
+  Feature("isProd()") {
 
-    scenario("empty string --> false") {
+    Scenario("empty string --> false") {
       EnvironmentUtil.isProd("") shouldBe false
     }
 
-    scenario("postfix begins with wrong delimiter --> false") {
+    Scenario("postfix begins with wrong delimiter --> false") {
       EnvironmentUtil.isProd(s"ubirch${EnvironmentUtil.POSTFIX_PROD.replace("-", "_")}") shouldBe false
     }
 
-    scenario("postfix is _-prod_ --> true") {
+    Scenario("postfix is _-prod_ --> true") {
       EnvironmentUtil.isProd(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe true
     }
 
-    scenario("postfix is _-demo_ --> false") {
+    Scenario("postfix is _-demo_ --> false") {
       EnvironmentUtil.isProd(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe false
     }
 
-    scenario("postfix is _-dev_ --> false") {
+    Scenario("postfix is _-dev_ --> false") {
       EnvironmentUtil.isProd(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe false
     }
 
-    scenario("postfix is _-local_ --> false") {
+    Scenario("postfix is _-local_ --> false") {
       EnvironmentUtil.isProd(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe false
     }
 
   }
 
-  feature("isNotProd()") {
+  Feature("isNotProd()") {
 
-    scenario("empty string --> true") {
-    EnvironmentUtil.isNotProd("") shouldBe true
-  }
+    Scenario("empty string --> true") {
+      EnvironmentUtil.isNotProd("") shouldBe true
+    }
 
-    scenario("postfix begins with wrong delimiter --> true") {
+    Scenario("postfix begins with wrong delimiter --> true") {
       EnvironmentUtil.isNotProd(s"ubirch${EnvironmentUtil.POSTFIX_PROD.replace("-", "_")}") shouldBe true
     }
 
-    scenario("postfix is _-prod_ --> false") {
+    Scenario("postfix is _-prod_ --> false") {
       EnvironmentUtil.isNotProd(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe false
     }
 
-    scenario("postfix is _-demo_ --> true") {
+    Scenario("postfix is _-demo_ --> true") {
       EnvironmentUtil.isNotProd(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe true
     }
 
-    scenario("postfix is _-dev_ --> true") {
+    Scenario("postfix is _-dev_ --> true") {
       EnvironmentUtil.isNotProd(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe true
     }
 
-    scenario("postfix is _-local_ --> true") {
+    Scenario("postfix is _-local_ --> true") {
       EnvironmentUtil.isNotProd(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe true
     }
 
   }
 
-  feature("isDemo()") {
+  Feature("isDemo()") {
 
-    scenario("empty string --> false") {
+    Scenario("empty string --> false") {
       EnvironmentUtil.isDemo("") shouldBe false
     }
 
-    scenario("postfix begins with wrong delimiter --> false") {
+    Scenario("postfix begins with wrong delimiter --> false") {
       EnvironmentUtil.isDemo(s"ubirch${EnvironmentUtil.POSTFIX_DEMO.replace("-", "_")}") shouldBe false
     }
 
-    scenario("postfix is _-prod_ --> false") {
+    Scenario("postfix is _-prod_ --> false") {
       EnvironmentUtil.isDemo(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe false
     }
 
-    scenario("postfix is _-demo_ --> true") {
+    Scenario("postfix is _-demo_ --> true") {
       EnvironmentUtil.isDemo(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe true
     }
 
-    scenario("postfix is _-dev_ --> false") {
+    Scenario("postfix is _-dev_ --> false") {
       EnvironmentUtil.isDemo(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe false
     }
 
-    scenario("postfix is _-local_ --> false") {
+    Scenario("postfix is _-local_ --> false") {
       EnvironmentUtil.isDemo(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe false
     }
 
   }
 
-  feature("isNotDemo()") {
+  Feature("isNotDemo()") {
 
-    scenario("empty string --> true") {
-    EnvironmentUtil.isNotDemo("") shouldBe true
-  }
+    Scenario("empty string --> true") {
+      EnvironmentUtil.isNotDemo("") shouldBe true
+    }
 
-    scenario("postfix begins with wrong delimiter --> true") {
+    Scenario("postfix begins with wrong delimiter --> true") {
       EnvironmentUtil.isNotDemo(s"ubirch${EnvironmentUtil.POSTFIX_DEMO.replace("-", "_")}") shouldBe true
     }
 
-    scenario("postfix is _-prod_ --> true") {
+    Scenario("postfix is _-prod_ --> true") {
       EnvironmentUtil.isNotDemo(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe true
     }
 
-    scenario("postfix is _-demo_ --> false") {
+    Scenario("postfix is _-demo_ --> false") {
       EnvironmentUtil.isNotDemo(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe false
     }
 
-    scenario("postfix is _-dev_ --> true") {
+    Scenario("postfix is _-dev_ --> true") {
       EnvironmentUtil.isNotDemo(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe true
     }
 
-    scenario("postfix is _-local_ --> true") {
+    Scenario("postfix is _-local_ --> true") {
       EnvironmentUtil.isNotDemo(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe true
     }
 
   }
 
-  feature("isDev()") {
+  Feature("isDev()") {
 
-    scenario("empty string --> false") {
+    Scenario("empty string --> false") {
       EnvironmentUtil.isDev("") shouldBe false
     }
 
-    scenario("postfix begins with wrong delimiter --> false") {
+    Scenario("postfix begins with wrong delimiter --> false") {
       EnvironmentUtil.isDev(s"ubirch${EnvironmentUtil.POSTFIX_DEV.replace("-", "_")}") shouldBe false
     }
 
-    scenario("postfix is _-prod_ --> false") {
+    Scenario("postfix is _-prod_ --> false") {
       EnvironmentUtil.isDev(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe false
     }
 
-    scenario("postfix is _-demo_ --> false") {
+    Scenario("postfix is _-demo_ --> false") {
       EnvironmentUtil.isDev(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe false
     }
 
-    scenario("postfix is _-dev_ --> true") {
+    Scenario("postfix is _-dev_ --> true") {
       EnvironmentUtil.isDev(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe true
     }
 
-    scenario("postfix is _-local_ --> false") {
+    Scenario("postfix is _-local_ --> false") {
       EnvironmentUtil.isDev(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe false
     }
 
   }
 
-  feature("isNotDev()") {
+  Feature("isNotDev()") {
 
-    scenario("empty string --> true") {
-    EnvironmentUtil.isNotDev("") shouldBe true
-  }
+    Scenario("empty string --> true") {
+      EnvironmentUtil.isNotDev("") shouldBe true
+    }
 
-    scenario("postfix begins with wrong delimiter --> true") {
+    Scenario("postfix begins with wrong delimiter --> true") {
       EnvironmentUtil.isNotDev(s"ubirch${EnvironmentUtil.POSTFIX_DEV.replace("-", "_")}") shouldBe true
     }
 
-    scenario("postfix is _-prod_ --> true") {
+    Scenario("postfix is _-prod_ --> true") {
       EnvironmentUtil.isNotDev(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe true
     }
 
-    scenario("postfix is _-demo_ --> true") {
+    Scenario("postfix is _-demo_ --> true") {
       EnvironmentUtil.isNotDev(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe true
     }
 
-    scenario("postfix is _-dev_ --> false") {
+    Scenario("postfix is _-dev_ --> false") {
       EnvironmentUtil.isNotDev(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe false
     }
 
-    scenario("postfix is _-local_ --> true") {
+    Scenario("postfix is _-local_ --> true") {
       EnvironmentUtil.isNotDev(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe true
     }
 
   }
 
-  feature("isLocalDev()") {
+  Feature("isLocalDev()") {
 
-    scenario("empty string --> false") {
+    Scenario("empty string --> false") {
       EnvironmentUtil.isLocal("") shouldBe false
     }
 
-    scenario("postfix begins with wrong delimiter --> false") {
+    Scenario("postfix begins with wrong delimiter --> false") {
       EnvironmentUtil.isLocal(s"ubirch${EnvironmentUtil.POSTFIX_DEV.replace("-", "_")}") shouldBe false
     }
 
-    scenario("postfix is _-prod_ --> false") {
+    Scenario("postfix is _-prod_ --> false") {
       EnvironmentUtil.isLocal(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe false
     }
 
-    scenario("postfix is _-demo_ --> false") {
+    Scenario("postfix is _-demo_ --> false") {
       EnvironmentUtil.isLocal(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe false
     }
 
-    scenario("postfix is _-dev_ --> false") {
+    Scenario("postfix is _-dev_ --> false") {
       EnvironmentUtil.isLocal(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe false
     }
 
-    scenario("postfix is _-local_ --> true") {
+    Scenario("postfix is _-local_ --> true") {
       EnvironmentUtil.isLocal(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe true
     }
 
   }
 
-  feature("isNotLocal()") {
+  Feature("isNotLocal()") {
 
-    scenario("empty string --> true") {
-    EnvironmentUtil.isNotLocal("") shouldBe true
-  }
+    Scenario("empty string --> true") {
+      EnvironmentUtil.isNotLocal("") shouldBe true
+    }
 
-    scenario("postfix begins with wrong delimiter --> true") {
+    Scenario("postfix begins with wrong delimiter --> true") {
       EnvironmentUtil.isNotLocal(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL.replace("-", "_")}") shouldBe true
     }
 
-    scenario("postfix is _-prod_ --> true") {
+    Scenario("postfix is _-prod_ --> true") {
       EnvironmentUtil.isNotLocal(s"ubirch${EnvironmentUtil.POSTFIX_PROD}") shouldBe true
     }
 
-    scenario("postfix is _-demo_ --> true") {
+    Scenario("postfix is _-demo_ --> true") {
       EnvironmentUtil.isNotLocal(s"ubirch${EnvironmentUtil.POSTFIX_DEMO}") shouldBe true
     }
 
-    scenario("postfix is _-dev_ --> true") {
+    Scenario("postfix is _-dev_ --> true") {
       EnvironmentUtil.isNotLocal(s"ubirch${EnvironmentUtil.POSTFIX_DEV}") shouldBe true
     }
 
-    scenario("postfix is _-local_ --> false") {
+    Scenario("postfix is _-local_ --> false") {
       EnvironmentUtil.isNotLocal(s"ubirch${EnvironmentUtil.POSTFIX_LOCAL}") shouldBe false
     }
 
